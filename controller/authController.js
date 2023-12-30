fbb77d89a0f41154e56c660e13286bf88bbea058
Lexgitar <berdea.alex@yahoo.com>
2023-12-30 00:00:38 +0000

    login user

--- controller/authController.js
+++ controller/authController.js
@@ -105,7 +105,7 @@ const login_post = async (req, res) => {
       const user = await User.login(email, password);
       const token = createToken(user._id);
       res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000});
-      res.status(200).json({user:user._id})
+      res.status(200).json({user})
     } catch (error) {
       const errors = handleErrors(error)
       res.status(400).json({errors})
