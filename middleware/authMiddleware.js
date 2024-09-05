649ba3661f1f9250bcdf73078bec94a79ce9a484
Lexgitar <berdea.alex@yahoo.com>
2024-09-06 00:32:18 +0100

    reqsth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -28,7 +28,7 @@ const requireAuth = (req, res, next) => {
             })
         } else {
             // res.send('require auth - error2', token)
-            res.status(500).send('require auth - error2 n tkn' + token + req)
+            res.send('require auth eor- error2 n tkn' + token + req)
             console.log('require auth - error2 n tkn ' + token)
             console.log('require auth - error2 n cuk ' + req.cookies)
         }
