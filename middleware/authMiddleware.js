14639626de0313682ebeabc85b3d21f07b831d13
Lexgitar <berdea.alex@yahoo.com>
2024-09-05 21:38:44 +0100

    omit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -12,48 +12,32 @@ const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
 
     //check if token / valid
-    try {
-        if (token) {
-            jwt.verify(token, jwtSecret, (err, decodedToken) => {
-                if (err) {
-                    console.log(err.message)
-                    // res.send('require auth - error1', err)
-                    res.status(202).send('require auth - error1', err)
-                } else {
-                    console.log('decoded ', decodedToken)
+    // try {
+    //     if (token) {
+    //         jwt.verify(token, jwtSecret, (err, decodedToken) => {
+    //             if (err) {
+    //                 console.log(err.message)
+    //                 // res.send('require auth - error1', err)
+    //                 res.status(202).send('require auth - error1', err)
+    //             } else {
+    //                 console.log('decoded ', decodedToken)
 
-                    next()
-                }
-            })
-        } else {
-            // res.send('require auth - error2', token)
-            res.status(500).send('require auth - error2 n tkn' + token + req)
-            console.log('require auth - error2 n tkn ' + token)
-            console.log('require auth - error2 n cuk ' + req.cookies)
-        }
+    //                 next()
+    //             }
+    //         })
+    //     } else {
+    //         // res.send('require auth - error2', token)
+    //         res.status(500).send('require auth - error2 n tkn' + token + req)
+    //         console.log('require auth - error2 n tkn ' + token)
+    //         console.log('require auth - error2 n cuk ' + req.cookies)
+    //     }
 
-    } catch (error) {
-        console.log('token eror', error)
-    }
-
-    if (token) {
-        jwt.verify(token, jwtSecret, (err, decodedToken) => {
-            if (err) {
-                console.log(err.message)
-                // res.send('require auth - error1', err)
-                res.status(202).send('require auth - error1', err)
-            } else {
-                console.log('decoded ', decodedToken)
-
-                next()
-            }
-        })
-    } else {
-        // res.send('require auth - error2', token)
-        res.status(500).send('require auth - error2 n tkn' + token + req)
-        console.log('require auth - error2 n tkn ' + token)
-        console.log('require auth - error2 n cuk ' + req.cookies)
-    }
+    // } catch (error) {
+    //     console.log('token eror', error)
+    //     res.status(500).send('require auth - error2 n tkn', error)
+    // }
+next()
+    
 }
 
 const requireAuthNRole = (req, res, next) => {
